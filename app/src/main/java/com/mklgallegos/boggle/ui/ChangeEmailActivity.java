4e58b0e0de8ff18a5120e139554e01fc33546927
Michael Gallegos <mklgallegos@gmail.com>
2016-05-10 11:21:55 -0700

    add ChangeEmailActivity

---
+++ app/src/main/java/com/mklgallegos/boggle/ui/ChangeEmailActivity.java
@@ -0,0 +1,15 @@
+package com.mklgallegos.boggle.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mklgallegos.boggle.R;
+
+public class ChangeEmailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_email);
+    }
+}
