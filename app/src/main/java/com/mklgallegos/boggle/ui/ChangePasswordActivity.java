7410bf6698c887b2d7f287c4eaa126c2d54ad5a3
Mike Gallegos <mklgallegos@gmail.com>
2016-05-11 11:05:00 -0700

    changePassword functionality implemented

--- app/src/main/java/com/mklgallegos/boggle/ui/ChangePasswordActivity.java
+++ app/src/main/java/com/mklgallegos/boggle/ui/ChangePasswordActivity.java
@@ -1,12 +1,16 @@
 package com.mklgallegos.boggle.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.mklgallegos.boggle.Constants;
 import com.mklgallegos.boggle.R;
 
@@ -14,6 +18,8 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class ChangePasswordActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = ChangePasswordActivity.class.getSimpleName();
 
     //Firebase
     private Firebase mFirebaseRef;
@@ -51,7 +57,31 @@ public class ChangePasswordActivity extends AppCompatActivity implements View.On
                 final String oldPassword = mOldPasswordEditText.getText().toString();
                 final String newPassword = mNewPasswordEditText.getText().toString();
 
-                //password change code here...
+
+                mFirebaseRef.changePassword(email, oldPassword, newPassword, new Firebase.ResultHandler() {
+
+
+
+                    @Override
+                    public void onSuccess() {
+
+                        //notify user password change was successful
+                        Toast.makeText(ChangePasswordActivity.this, "Password Change Successful!", Toast.LENGTH_LONG).show();
+
+                        //implement intent to transition user back to AccountActivity
+                        Intent intent = new Intent(ChangePasswordActivity.this, AccountActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+
+                    }
+
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+
+                    }
+
+                });
         }
 
 
